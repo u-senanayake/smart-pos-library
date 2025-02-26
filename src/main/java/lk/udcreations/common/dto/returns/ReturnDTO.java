a91723f608da510d009fe9cd593f81c3555cef6c
u-senanayake <udayanga@newindia.co.jp>
2025-02-26 14:47:04 +0900

    changed return DTO class

--- src/main/java/lk/udcreations/common/dto/returns/ReturnDTO.java
+++ src/main/java/lk/udcreations/common/dto/returns/ReturnDTO.java
@@ -2,12 +2,15 @@ package lk.udcreations.common.dto.returns;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import lk.udcreations.common.dto.salesitems.SalesItemDTO;
 
 public class ReturnDTO {
 
 	private Integer returnId;
 	private Integer saleId;
-	private Integer saleItemId;
+	private List<SalesItemDTO> salesItems;
 	private int quantity;
 	private String reason;
 	private BigDecimal refundAmount;
@@ -29,16 +32,16 @@ public class ReturnDTO {
 		this.saleId = saleId;
 	}
 
-	public Integer getSaleItemId() {
-		return saleItemId;
-	}
-
-	public void setSaleItemId(Integer saleItemId) {
-		this.saleItemId = saleItemId;
-	}
-
 	public int getQuantity() {
 		return quantity;
+	}
+
+	public List<SalesItemDTO> getSalesItems() {
+		return salesItems;
+	}
+
+	public void setSalesItems(List<SalesItemDTO> salesItems) {
+		this.salesItems = salesItems;
 	}
 
 	public void setQuantity(int quantity) {
